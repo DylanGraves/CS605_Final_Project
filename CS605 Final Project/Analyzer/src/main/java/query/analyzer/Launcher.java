a4831237d36488c65396e679f82ba863c8fd40e0
dtgraves <dylanthomasgraves@gmail.com>
2023-09-11 21:19:26 -0700

    added standalone executable, in case there is trouble compiling because of dependencies

---
+++ CS605 Final Project/Analyzer/src/main/java/query/analyzer/Launcher.java
@@ -0,0 +1,8 @@
+package query.analyzer;
+
+// This class exists as a workaround, because JavaFX doesn't like to be deployed as a jar executable.
+public class Launcher {
+    public static void main(String[] args) {
+        Program.main(args);
+    }
+}
